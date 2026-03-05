1eae43a9df54a6377e3e27aeef9ce4982be5ea2b
Mohammad <mohammad70623@gmail.com>
2026-03-06 03:26:27 +0600

    imported all necessery libraries?

---
+++ rag_chatbot1.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+
