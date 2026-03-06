8733c9dc9db22ff6953a9e00189d9ef2429e26ae
Mohammad <mohammad70623@gmail.com>
2026-03-06 12:40:34 +0600

    imported all necessery libraries

--- rag_chatbot1.py
+++ rag_chatbot1.py
@@ -8,4 +8,3 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
-
