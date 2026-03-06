f12285432b7b70844e741f73ffd2525a1f33b811
Mohammad <mohammad70623@gmail.com>
2026-03-06 12:42:37 +0600

    Load pdf file

--- rag_chatbot1.py
+++ rag_chatbot1.py
@@ -8,3 +8,17 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
+
+os.environ['LANGCHAIN_PROJECT'] = 'Rag Chatbot'
+load_dotenv()  
+
+DATA_PATH = "data/"
+def load_pdf_files(data): 
+    loader = DirectoryLoader(data, 
+                            glob = '*.pdf', 
+                            loader_cls = PyPDFLoader)
+
+    documents = loader.load() 
+    return documents
+documents = load_pdf_files(data=DATA_PATH)
+
